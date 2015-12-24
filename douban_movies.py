40327536abbe94b8d9c4889d543905303c6f3da2
Joey Huang <kamidox@gmail.com>
2015-12-24 09:10:01 +0800

    refactor code and fix bug

--- douban_movies.py
+++ douban_movies.py
@@ -2,7 +2,7 @@ import urllib2
 from HTMLParser import HTMLParser
 
 
-class HotMovieParser(HTMLParser):
+class MovieParser(HTMLParser):
     def __init__(self):
         HTMLParser.__init__(self)
         self.movies = []
@@ -14,7 +14,7 @@ class HotMovieParser(HTMLParser):
                     return attr[1]
             return None
 
-        if tag == 'li' and _attr_value(attrs, 'data-title') and _attr_value(attrs, 'data-score'):
+        if tag == 'li' and _attr_value(attrs, 'data-category') == 'nowplaying':
             movie = {}
             movie['title'] = _attr_value(attrs, 'data-title')
             movie['score'] = _attr_value(attrs, 'data-score')
@@ -24,8 +24,9 @@ class HotMovieParser(HTMLParser):
             print('%(title)s|%(score)s|%(director)s|%(actors)s' % movie)
             self.movies.append(movie)
 
-def hot_movies(url):
-    parser = HotMovieParser()
+
+def nowplaying_movies(url):
+    parser = MovieParser()
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.73 Safari/537.36'}
     req = urllib2.Request(url, headers=headers)
     parser.feed(urllib2.urlopen(req).read())
@@ -34,7 +35,7 @@ def hot_movies(url):
 
 if __name__ == '__main__':
     url = 'http://movie.douban.com/nowplaying/xiamen/'
-    movies = hot_movies(url)
+    movies = nowplaying_movies(url)
 
     import json
     print('%s' % json.dumps(movies, sort_keys=True, indent=4, separators=(',', ': ')))
