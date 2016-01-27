b199c7af6e22f5c91306ca3a5683b02a00546bcc
Joey Huang <kamidox@gmail.com>
2016-01-27 13:40:16 +0800

    tang shi spider v2

--- tangshi_list_v2.py
+++ tangshi_list_v2.py
@@ -3,6 +3,11 @@ import requests
 import re
 from HTMLParser import HTMLParser
 
+# ugly code to fix UnicodeDecodeError
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
 
 def _attr(attrs, attrname):
     for attr in attrs:
@@ -37,7 +42,6 @@ class PoemParser(HTMLParser):
 
     def handle_data(self, data):
         if self.in_a:
-            print(data)
             m = self.pattern.match(data)
             if m:
                 self.current_poem['title'] = m.group(1)
@@ -49,27 +53,20 @@ class PoemParser(HTMLParser):
 class PoemContentParser(HTMLParser):
     def __init__(self):
         HTMLParser.__init__(self)
-        self.content = ""
-        self.in_div = False
+        self.content = []
         self.in_p = False
 
     def handle_starttag(self, tag, attrs):
-        if tag == 'div' and _attr(attrs, 'class') == 'authorShow':
-            self.in_div = True
-        if tag == 'p' and self.in_div:
+        if tag == 'p' and _attr(attrs, 'align') == 'center':
             self.in_p = True
-            self.in_div = False     # 我们只关心 div 下的第一个 p 标签
 
     def handle_endtag(self, tag):
-        if tag == 'div':
-            self.in_div = False
-
         if tag == 'p':
             self.in_p = False
 
     def handle_data(self, data):
         if self.in_p:
-            print(data)
+            self.content.append(data)
 
 
 def retrive_tangshi_300():
@@ -84,7 +81,7 @@ def download_poem(poem):
     r = requests.get('http://www.gushiwen.org' + poem['url'])
     parser = PoemContentParser()
     parser.feed(r.content)
-    poem['content'] = parser.content
+    poem['content'] = '\n'.join(parser.content)
 
 
 if __name__ == '__main__':
@@ -94,6 +91,7 @@ if __name__ == '__main__':
         print('标题: %(title)s\t作者：%(author)s\tURL: %(url)s' % (l[i]))
 
     # download each poem
-    for i in range(10):
+    for i in range(len(l)):
+        print('#%d downloading poem from: %s' % (i, l[i]['url']))
         download_poem(l[i])
         print('标题: %(title)s\t作者：%(author)s\n%(content)s' % (l[i]))
